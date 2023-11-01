0cfa75fe92a98925c143bbaeef11a9f19be8a399
Kris Templin <kris.templin@gmail.com>
2023-11-01 11:44:34 -0700

    Update code to pass npm test

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ const printStacks = () => {
 
 // Next, what do you think this function should do?
 const movePiece = async (stack1, stack2) => {
-  if (checkForWin(true)){
+  if (checkForWin()){
     console.log('You\'ve won!!!')
     //logic to show number of moves used
     //logic to show optimal number of moves possible
@@ -65,16 +65,20 @@ const movePiece = async (stack1, stack2) => {
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
 const isLegal = (stack1, stack2) => {
-  const check1 = stacks[stack1].length-1
-  const check2 = stacks[stack2].length
+  // const check1 = stacks[stack1].length-1
+  // const check2 = stacks[stack2].length
+  const check1 = stacks[stack1][stacks[stack1].length-1]
+  const check2 = stacks[stack2][stacks[stack2].length-1]
 
   return check1 < check2 || stacks[stack2].length === 0
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  if(stacks["c"].length === 4){
+  if((stacks['b'].length == 4) || (stacks['c'].length == 4)){
     return true
+  } else {
+    return false
   }
 }
 
@@ -125,6 +129,14 @@ if (typeof describe === 'function') {
       stacks = {
         a: [4, 3],
         b: [2],
+        c: [1]
+      };
+      assert.equal(isLegal('c','b'), true);
+    });
+    it('should allow move of smaller ring onto larger', () => {
+      stacks = {
+        a: [4, 2],
+        b: [3],
         c: [1]
       };
       assert.equal(isLegal('c','b'), true);
